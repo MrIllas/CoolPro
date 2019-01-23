cdc5dc73c5b1f51e6b8af5d8344122f781ef7746
Robert Recorda <alu2015324@a22o26.local>
2019-01-23 20:18:49 +0100

    TabBar Backgrounds

---
+++ CoolPro/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  CoolPro
+//
+//  Created by Robert Recorda on 23/1/19.
+//  Copyright © 2019 RobertRecordà. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Common settings set up
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.lightGray], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.black], for: .normal)
+        
+        //Sets up every item of the tabBar
+        
+        tabBarItem = self.tabBar.items![0]
+        tabBarItem.image = UIImage(named: "totoro_black")?.withRenderingMode(.alwaysOriginal)
+        tabBarItem.selectedImage = UIImage(named: "totoro_grey")?.withRenderingMode(.alwaysOriginal)
+        
+        tabBarItem = self.tabBar.items![1]
+        tabBarItem.image = UIImage(named: "search_black")?.withRenderingMode(.alwaysOriginal)
+        tabBarItem.selectedImage = UIImage(named: "search_grey")?.withRenderingMode(.alwaysOriginal)
+        
+    }
+}
