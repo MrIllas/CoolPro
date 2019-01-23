8d8508c877754bad004d537dd9c18363d1adec9a
Robert Recorda <alu2015324@a22o26.local>
2019-01-23 19:06:57 +0100

    Initial Commit

---
+++ CoolPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolPro
+//
+//  Created by Robert Recorda on 23/1/19.
+//  Copyright © 2019 RobertRecordà. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
